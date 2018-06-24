0465effef5fd894f2a8f1704d64120fa7b59c86e
jettairlines <lwanghe2@gmail.com>
2018-06-24 21:02:21 +0800

    submit modal WIP, functional but ugly

--- src/SubmitForm/SubmitForm.jsx
+++ src/SubmitForm/SubmitForm.jsx
@@ -1,45 +1,91 @@
 import React, {Component} from 'react';
+import Modal from 'react-modal';
 import './SubmitForm.css';
+
+const modalCSS = {
+    content : {
+        height: '50vh',
+    }
+}
 
 class SubmitForm extends Component{
     constructor(props){
         super(props);
         this.state = {
             newQuestionContent: '',
+            isOpen: false,
+            status: "open",
         };
         this.handleUserInput = this.handleUserInput.bind(this);
         this.writeQuestion = this.writeQuestion.bind(this);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
     }
 
     handleUserInput(e){
         this.setState({
             newQuestionContent: e.target.value,
+            status: "open",
         })
     }
 
     writeQuestion(){
+        //check for null & empty string
+        console.log(this.state.newQuestionContent);
         if(this.state.newQuestionContent && this.state.newQuestionContent.trim()){
             //call method that sets questionContent for question to
             //value of input
-            this.props.addQuestion(this.state.newQuestionContent);
+//            this.props.addQuestion(this.state.newQuestionContent);
             //set back to empty string after add.
             this.setState({
+                status: "success",
                 newQuestionContent: '',
             })
+            
+        }else{
+            this.setState({ status: "error" });
         }
     }
+    
+    handleOpen(){
+        this.setState( {isOpen : true});
+    }
+    handleClose(){
+        this.setState({
+            newQuestionContent: null,
+            status: "open",
+            isOpen:false,
+        });
+    }
+    
     render(){
-        return(
-            <div className="submitWrapper">
-                <input className="questionInput"
-                placeholder="Have a good question?"
-                value={this.state.newQuestionContent}
-                onChange={this.handleUserInput} />
-                <button className="submitButton"
-                onClick={this.writeQuestion}>Add</button>
+        var {newQuestionContent, isOpen, status} = this.state;
+        return( <div>
+            <a onClick={this.handleOpen}>Have a good submit</a>
+            
+            <Modal isOpen={isOpen} onRequestClose={this.handleClose} className="submitModal" overlayClassName="submitModalOverlay">
                 
+                <span id="close" onClick={this.handleClose}><i className="fas fa-times"></i></span>
+
+            {(status === "open" || status === "error") && (<div>
+                <textarea className="questionInput"
+                value={newQuestionContent}
+                placeholder="Submit your own question here..."
+                onChange={this.handleUserInput}/>
+                
+                {(status === "open") && (<button className="submitButton"
+            onClick={this.writeQuestion}>Submit</button>)}
+                    {(status === "error") && <p>There was an error submitting your question :(</p>}
+                </div>)}
+            
+            {(status === "success") && (<div>
+                <p>Submission Succesfull!</p><a>submit another one</a>
+                </div>)}
+                    
+                    
+                </Modal>
             </div>
-        )
+        );
     }
 }
 
